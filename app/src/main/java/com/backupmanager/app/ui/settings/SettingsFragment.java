9cf1a7724e29e7748e86b9e1814e470d8f3d1026
stefanmih <bjtnpn4tgrepolis@gmail.com>
2022-12-27 02:33:48 +0100

    Added settings

--- app/src/main/java/com/backupmanager/app/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/backupmanager/app/ui/settings/SettingsFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -15,6 +17,8 @@ import androidx.lifecycle.ViewModelProvider;
 import com.backupmanager.app.R;
 import com.backupmanager.app.databinding.FragmentSettingsBinding;
 import com.backupmanager.app.utils.Configuration;
+
+import java.util.ArrayList;
 
 public class SettingsFragment extends Fragment {
 
@@ -33,6 +37,18 @@ public class SettingsFragment extends Fragment {
         binding.autoLogin.setChecked(Configuration.getConfiguration(getContext()).getParameterValue("autologin").equals("true"));
         binding.autoLogin.setOnCheckedChangeListener((compoundButton, b) -> {
             Configuration.getConfiguration(getContext()).addOrModifyConfiguration("autologin", compoundButton.isChecked() ? "true" : "false");
+        });
+        binding.spinner.setEnabled(binding.switch1.isChecked());
+        binding.textView13.setEnabled(binding.switch1.isChecked());
+        binding.textView14.setEnabled(binding.switch1.isChecked());
+        binding.spinner.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, new String[]{"Daily", "Weekly", "Monthly"}));
+        binding.switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                binding.textView13.setEnabled(compoundButton.isChecked());
+                binding.textView14.setEnabled(compoundButton.isChecked());
+                binding.spinner.setEnabled(compoundButton.isChecked());
+            }
         });
         return root;
     }
