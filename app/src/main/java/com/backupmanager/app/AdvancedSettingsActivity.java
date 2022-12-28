adb7006dd0b7d5858e2af9dbfb345ef786bac2b6
stefanmih <bjtnpn4tgrepolis@gmail.com>
2022-12-28 16:50:57 +0100

    Added qr code scanning

--- app/src/main/java/com/backupmanager/app/AdvancedSettingsActivity.java
+++ app/src/main/java/com/backupmanager/app/AdvancedSettingsActivity.java
@@ -1,13 +1,20 @@
 package com.backupmanager.app;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.backupmanager.app.utils.CaptureAct;
 import com.backupmanager.app.utils.Configuration;
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class AdvancedSettingsActivity extends AppCompatActivity {
 
@@ -30,5 +37,23 @@ public class AdvancedSettingsActivity extends AppCompatActivity {
 
             }
         });
+        findViewById(R.id.qrcode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ScanOptions options = new ScanOptions();
+                options.setBeepEnabled(true);
+                options.setOrientationLocked(true);
+                options.setCaptureActivity(CaptureAct.class);
+                barLauncher.launch(options);
+            }
+        });
     }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            ((EditText)findViewById(R.id.textIp)).setText(result.getContents().split(":")[0]);
+            ((EditText)findViewById(R.id.textHttp)).setText(result.getContents().split(":")[1]);
+            ((EditText)findViewById(R.id.textTcp)).setText(result.getContents().split(":")[2]);
+        }
+    });
 }
---
+++ app/src/main/java/com/backupmanager/app/utils/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.backupmanager.app.utils;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
